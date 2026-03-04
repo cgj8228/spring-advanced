3372da277840778ca1beb85a3bdb0053bec3bf3c
최길중 <choil8228@gmail.com>
2026-03-04 10:52:51 +0900

    2.3번 수정

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -16,7 +16,7 @@ public class UserChangePasswordRequest {
     private String oldPassword;
     @NotBlank(message = "새 비밀번호는 필수입니다.")
     @Size(min = 8, message = "새 비밀번호는 8자 이상이어야 합니다.")
-    @Pattern(regexp = ".*[0~9].*", message = "새 비밀번호는 숫자를 포함해야 합니다.")
+    @Pattern(regexp = ".*[0-9].*", message = "새 비밀번호는 숫자를 포함해야 합니다.")
     @Pattern(regexp = ".*[A-Z].*", message = "새 비밀번호는 대문자를 포함해야 합니다.")
     private String newPassword;
 }
